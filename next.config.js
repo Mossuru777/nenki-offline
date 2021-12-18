3a5e42b32196da4db4f7b944263003f1eea05c9d
Mossan <mossuru777@gmail.com>
2021-12-19 00:05:03 +0900

    👷 Configure GitHub Actions to build the app as a static page and publish it on GitHub Pages.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const urlPrefix = process.env.URL_PREFIX ? `/${process.env.URL_PREFIX}` : "";
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
@@ -8,5 +10,9 @@ module.exports = {
       fs: false
     }
     return config;
-  }
+  },
+
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true
 }
