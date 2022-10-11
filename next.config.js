89da7602084e62c37c310849590530f7a767769c
Mossan <mossuru777@gmail.com>
2022-10-12 00:01:18 +0900

    ⬆ Update dependencies.

--- next.config.js
+++ next.config.js
@@ -1,24 +1,22 @@
-const withPWA = require("next-pwa");
-const urlPrefix = process.env.URL_PREFIX ? `/${process.env.URL_PREFIX}` : "";
+const withPWA = require("next-pwa")({
+    dest: "public",
+    disable: process.env.NODE_ENV === "development"
+});
+const urlPrefix = process.env.URL_PREFIX ? `/${process.env.URL_PREFIX}` : undefined;
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
-  reactStrictMode: true,
+    reactStrictMode: true,
 
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false
-    }
-    return config;
-  },
+    webpack: (config) => {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            fs: false
+        }
+        return config;
+    },
 
-  assetPrefix: urlPrefix,
-  basePath: urlPrefix,
-  trailingSlash: true,
-
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development"
-  }
+    assetPrefix: urlPrefix,
+    basePath: urlPrefix,
+    trailingSlash: true
 });
