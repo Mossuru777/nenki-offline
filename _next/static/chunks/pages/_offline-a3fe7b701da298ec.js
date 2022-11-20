32d76c56eb42713eafbe4aceac561e5c1fc2b460
Mossuru777 <Mossuru777@users.noreply.github.com>
2022-11-20 10:25:27 +0000

    deploy: 3cd4d3cc6b9bad5b59b831ea63f562f15b8f4fad

---
+++ _next/static/chunks/pages/_offline-a3fe7b701da298ec.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[95],{4927:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_offline",function(){return _(9369)}])},9369:function(n,u,_){"use strict";_.r(u);var f=_(5149);u.default=f.default}},function(n){n.O(0,[774,678,885,630,752,579,340,791,447,149,888,179],(function(){return u=4927,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
